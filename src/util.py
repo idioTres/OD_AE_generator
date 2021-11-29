8f6ee6bddd2a6106d5d6d075789603e9a2db07f2
meo-s <kmeos1579@gmail.com>
2021-11-30 01:58:00 +0900

    Fix incorrect code

--- src/util.py
+++ src/util.py
@@ -6,8 +6,8 @@ def xywh2xyxy(xywh: torch.Tensor) -> torch.Tensor:
   xyxy = xywh.clone().detach_()
   xyxy[..., 0] = xywh[..., 0] - xywh[..., 2] / 2
   xyxy[..., 1] = xywh[..., 1] - xywh[..., 3] / 2
-  xyxy[..., 2] = xywh[..., 2] + xywh[..., 2] / 2
-  xyxy[..., 3] = xywh[..., 3] + xywh[..., 3] / 2
+  xyxy[..., 2] = xywh[..., 0] + xywh[..., 2] / 2
+  xyxy[..., 3] = xywh[..., 1] + xywh[..., 3] / 2
   return xyxy
 
 
