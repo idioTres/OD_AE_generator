cc2cbac13b73a723f896dd5d0d69c16e34fc78fd
meo-s <kmeos1579@gmail.com>
2021-11-30 00:09:02 +0900

    Add AppDelegate class

--- gen.py
+++ gen.py
@@ -1,12 +1,18 @@
 from argparse import ArgumentParser
 
 
+class AppDelegate(object):
+
+  def __init__(self):
+    super().__init__()
+
+
 if __name__ == '__main__':
   arg_parser = ArgumentParser()
   arg_parser.add_argument('-i', '--image', dest='image_path', type=str, required=True,
                           help='''
                           A path to source image which will be used to generate adversarial example.''')
-  arg_parser.add_argument('-m', '--model', dest='model', type=str,
+  arg_parser.add_argument('-m', '--model', dest='model_type', type=str,
                           help='''
                           A type of model which is victim of adversarial attack.
                           It it the reserved argument, so regardless of its value, YOLOv5 is used to generate adversarial example.''')
