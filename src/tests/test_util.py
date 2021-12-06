07668426d62a4ca6e45482b4441ad17ea77955d1
meo-s <kmeos1579@gmail.com>
2021-12-07 00:03:25 +0900

    Add test_mksquare()

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 
 from .. import util
 
@@ -19,3 +18,19 @@ def test_query_dict():
   without default value
   '''
   assert util.query_dict({}, '') is None
+
+
+def test_mksquare():
+  for initial_size in [(10, 1), (1, 10)]:
+    img = np.zeros(initial_size)
+    img = util.mksquare(img, pad_color=(0, 0, 0), size=(100, 100))
+    assert np.all(img == np.zeros((100, 100)))
+
+  pad_color = (255, )
+  img = np.zeros((4, 8))
+  img = util.mksquare(img, pad_color)
+
+  pad = np.full((2, 8), fill_value=pad_color)
+  target_img = np.concatenate([pad, np.zeros((4, 8)), pad], axis=0)
+
+  assert np.all(img == target_img)
