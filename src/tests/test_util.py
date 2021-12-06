f0d379153e158ce74852bf7e8a8b6ffa6cd80a4d
meo-s <kmeos1579@gmail.com>
2021-12-06 23:49:49 +0900

    Change order of test functions

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -3,8 +3,10 @@ import pytest
 from .. import util
 
 
-def test_query_dict_without_default_value():
-  assert util.query_dict({}, '') is None
+def test_query_dict():
+  key = 'key'
+  some_dict = dict([[key, 'value']])
+  assert util.query_dict(some_dict, key, 'not this') == some_dict[key]
 
 
 def test_query_dict_with_default_value():
@@ -12,7 +14,5 @@ def test_query_dict_with_default_value():
   assert util.query_dict({}, '', default_value) == default_value
 
 
-def test_query_dict():
-  key = 'key'
-  some_dict = dict([[key, 'value']])
-  assert util.query_dict(some_dict, key, 'not this') == some_dict[key]
+def test_query_dict_without_default_value():
+  assert util.query_dict({}, '') is None
