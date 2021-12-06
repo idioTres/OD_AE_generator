f6d39f8b396a26433f5b14ddad7c91d509595add
meo-s <kmeos1579@gmail.com>
2021-12-06 23:46:26 +0900

    Add test_query_dict_with_default_value()

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -5,3 +5,8 @@ from .. import util
 
 def test_query_dict_without_default_value():
   assert util.query_dict({}, '') is None
+
+
+def test_query_dict_with_default_value():
+  default_value = 'default_value'
+  assert util.query_dict({}, '', default_value) == default_value
